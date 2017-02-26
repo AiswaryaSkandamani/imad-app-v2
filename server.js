bda11feccf5be70fdf9ecd53d14a1f2ca8f41e0b
Aiswarya.S <savi.rosee@gmail.com>
2017-02-26 16:24:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.get('/', function (req, res) {
 app.get('/ui/article-one.html',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article-one.html'));
 });
-app.get('/article-two',function(req,res){
-    res.send('the requested article two is fetched');
+app.get('/ui/article-two.html',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','article-two.html'));
 });
-app.get('/article-three',function(req,res){
-    res.send('the requested article three is fetched');
+app.get('/ui/article-three',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
